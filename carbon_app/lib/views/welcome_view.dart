c481e3e01fe9fbf996e8c61721c10625636b8f62
Odréanne Breton <odreanne.breton@gmail.com>
2024-02-03 16:08:39 -0500

    refer

--- carbon_app/lib/views/welcome_view.dart
+++ carbon_app/lib/views/welcome_view.dart
@@ -40,54 +40,54 @@ class _WelcomeViewState extends State<WelcomeView> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          iconSize: 45,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.checklist_rtl),
-              label: 'to do',
-              backgroundColor: Color.fromARGB(255, 54, 212, 244),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarm),
-              label: 'shrudy',
-              backgroundColor: Color.fromARGB(255, 212, 1, 212),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.textsms),
-              label: 'notification',
-              backgroundColor: Colors.purple,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'profile',
-              backgroundColor: Colors.pink,
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Color.fromARGB(255, 236, 104, 37),
-          onTap: (int index) {
-            switch (index) {
-              case 0:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(todosRoute, (route) => false);
-              case 1:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(shrudyRoute, (route) => false);
-              case 2:
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                    notificationsRoute, (route) => false);
+      // bottomNavigationBar: BottomNavigationBar(
+      //     iconSize: 45,
+      //     items: const <BottomNavigationBarItem>[
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.checklist_rtl),
+      //         label: 'to do',
+      //         backgroundColor: Color.fromARGB(255, 54, 212, 244),
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.access_alarm),
+      //         label: 'shrudy',
+      //         backgroundColor: Color.fromARGB(255, 212, 1, 212),
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.textsms),
+      //         label: 'notification',
+      //         backgroundColor: Colors.purple,
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.account_circle),
+      //         label: 'profile',
+      //         backgroundColor: Colors.pink,
+      //       ),
+      //     ],
+      //     currentIndex: _selectedIndex,
+      //     selectedItemColor: Color.fromARGB(255, 236, 104, 37),
+      //     onTap: (int index) {
+      //       switch (index) {
+      //         case 0:
+      //           Navigator.of(context)
+      //               .pushNamedAndRemoveUntil(todosRoute, (route) => false);
+      //         case 1:
+      //           Navigator.of(context)
+      //               .pushNamedAndRemoveUntil(shrudyRoute, (route) => false);
+      //         case 2:
+      //           Navigator.of(context).pushNamedAndRemoveUntil(
+      //               notificationsRoute, (route) => false);
 
-              case 3:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(profileRoute, (route) => false);
-            }
-            setState(
-              () {
-                _selectedIndex = index;
-              },
-            );
-          }),
+      //         case 3:
+      //           Navigator.of(context)
+      //               .pushNamedAndRemoveUntil(profileRoute, (route) => false);
+      //       }
+      //       setState(
+      //         () {
+      //           _selectedIndex = index;
+      //         },
+      //       );
+      //     }),
     );
   }
 }
