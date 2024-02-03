8ea321812f1c926c49b5c53746fc56e616bc1b42
Julien Racette <julienracette@pop-os.localdomain>
2024-02-03 18:29:14 -0500

    ajout des constantes de couleurs

---
+++ carbon_app/lib/constants/color.dart
@@ -0,0 +1,9 @@
+//https://visme.co/blog/fr/couleurs-site-web/
+//Cool et frais
+import 'package:flutter/material.dart';
+
+const background_color = Color(0xffdef4c6);
+const header_color = Color(0xff73e2a7);
+const letter_color = Color(0xff1b512d);
+const navbar_color = Color(0xff1c7c54);
+const icon_color = Color(0xffb1cf5f);
