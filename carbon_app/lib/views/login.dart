35442af243d0bae9230a655f384cf3a3b67fd387
Odréanne Breton <odreanne.breton@gmail.com>
2024-02-04 03:31:24 -0500

    log in ui!

--- carbon_app/lib/views/login.dart
+++ carbon_app/lib/views/login.dart
@@ -18,95 +18,106 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              height: 300,
-              width: 300,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                controller: _emailController,
-                decoration: const InputDecoration(hintText: 'Email'),
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            OutlinedButton(
-              onPressed: () {
-                // Handle register button press
-                Navigator.of(context).pushNamed(loginRoute);
-              },
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(color: Color.fromARGB(255, 65, 154, 40)),
-                minimumSize: const Size(170, 60), // Adjust the size as needed
-              ),
-              child: Text(
-                'Login',
-                style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                  color: Color.fromARGB(255, 37, 129, 37),
-                  fontSize: 28, // Adjust the font size
-                )),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final message = await AuthService().login(
-                  email: _emailController.text,
-                  password: _passwordController.text,
-                );
-                if (message!.contains('Success')) {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) => const WelcomeView(),
-                    ),
-                  );
-                }
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(message),
-                  ),
-                );
-              },
-              child: const Text('Login'),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const CreateAccount(),
-                  ),
-                );
-              },
-              child: const Text('Create Account'),
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Center(
+          child: Column(children: [
+            Center(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    height: 150,
+                    width: 150,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(hintText: 'Email'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  OutlinedButton(
+                    onPressed: () async {
+                      final message = await AuthService().login(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      );
+                      if (message!.contains('Success')) {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const WelcomeView(),
+                          ),
+                        );
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(message),
+                        ),
+                      );
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 65, 154, 40)),
+                      minimumSize:
+                          const Size(125, 50), // Adjust the size as needed
+                    ),
+                    child: Text(
+                      'Login',
+                      style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 37, 129, 37),
+                        fontSize: 25, // Adjust the font size
+                      )),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const CreateAccount(),
+                        ),
+                      );
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 65, 154, 40)),
+                      minimumSize:
+                          const Size(170, 50), // Adjust the size as needed
+                    ),
+                    child: Text(
+                      'Create a new account',
+                      style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 37, 129, 37),
+                        fontSize: 25, // Adjust the font size
+                      )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ));
   }
 }
