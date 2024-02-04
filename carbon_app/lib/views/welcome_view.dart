0106d04ceeeda5e40b8df6f235da42e9d2954138
Odréanne Breton <odreanne.breton@gmail.com>
2024-02-04 01:06:04 -0500

    woww le ui est beauuu

--- carbon_app/lib/views/welcome_view.dart
+++ carbon_app/lib/views/welcome_view.dart
@@ -38,150 +38,161 @@ class _WelcomeViewState extends State<WelcomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70.0),
-        child: AppBar(
-          backgroundColor: headerColor,
-          foregroundColor: Color.fromARGB(255, 0, 0, 0),
-          title: Center(
-            child: Text(
-              'GREENRIDE',
-              style: GoogleFonts.lato(
-                textStyle: const TextStyle(
-                  color: letterColor,
-                  fontSize: 40,
-                  fontWeight: FontWeight.normal,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70.0),
+          child: AppBar(
+            backgroundColor: headerColor,
+            foregroundColor: Color.fromARGB(255, 0, 0, 0),
+            title: Center(
+              child: Text(
+                'GREENRIDE',
+                style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    color: letterColor,
+                    fontSize: 40,
+                    fontWeight: FontWeight.normal,
+                  ),
                 ),
               ),
             ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: 3,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(children: [
-                  const SizedBox(height: 10),
-                  Container(
-                    height: 50,
-                    color: postColor,
-                    child: Row(children: [
-                      const SizedBox(width: 20),
-                      Text(userDashboard[index]["user"],
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.roboto(
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          )),
-                      const Spacer(),
-                      Text(userDashboard[index]["Carbon"],
-                          textAlign: TextAlign.right,
-                          style: GoogleFonts.roboto(
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          )),
-                      const SizedBox(width: 20),
-                    ]),
-                  ),
-                  Container(
-                    color: postColor,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 20),
-                        Expanded(
-                            child: Center(
-                          child: Text(userDashboard[index]["text"],
-                              textAlign: TextAlign.center,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              opacity: 0.9,
+              image: AssetImage("assets/grass.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(children: [
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 50,
+                        color: postColor,
+                        child: Row(children: [
+                          const SizedBox(width: 20),
+                          Text(userDashboard[index]["user"],
+                              textAlign: TextAlign.left,
                               style: GoogleFonts.roboto(
                                 textStyle: const TextStyle(
-                                  fontSize: 23,
+                                  fontSize: 20,
                                   fontWeight: FontWeight.normal,
                                 ),
                               )),
-                        )),
-                        const SizedBox(width: 20),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    color: postColor,
-                    child: const Row(
-                      children: [
-                        SizedBox(height: 20),
-                      ],
-                    ),
-                  ),
-                ]);
-                // FeedPost(
-                //               newKey: userDashboard[index]["newKey"],
-                //               name: userDashboard[index]["name"],
-                //               carbon: userDashboard[index]["carbon"],
-                //               text: userDashboard[index]["text"],
-                //               time: userDashboard[index]["time"]);
-              },
+                          const Spacer(),
+                          Text(userDashboard[index]["Carbon"],
+                              textAlign: TextAlign.right,
+                              style: GoogleFonts.roboto(
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              )),
+                          const SizedBox(width: 20),
+                        ]),
+                      ),
+                      Container(
+                        color: postColor,
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 20),
+                            Expanded(
+                                child: Center(
+                              child: Text(userDashboard[index]["text"],
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.roboto(
+                                    textStyle: const TextStyle(
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  )),
+                            )),
+                            const SizedBox(width: 20),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: postColor,
+                        child: const Row(
+                          children: [
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                    ]);
+                    // FeedPost(
+                    //               newKey: userDashboard[index]["newKey"],
+                    //               name: userDashboard[index]["name"],
+                    //               carbon: userDashboard[index]["carbon"],
+                    //               text: userDashboard[index]["text"],
+                    //               time: userDashboard[index]["time"]);
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          iconSize: 45,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.grass_outlined),
-              label: 'me',
-              backgroundColor: navbarColor,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bar_chart),
-              label: 'others',
-              backgroundColor: navbarColor,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.emoji_objects),
-              label: 'knowledge',
-              backgroundColor: navbarColor,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'profile',
-              backgroundColor: navbarColor,
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: iconColor,
-          onTap: (int index) {
-            switch (index) {
-              case 0:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(myStats, (route) => false);
-              case 1:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(welcomeRoute, (route) => false);
-              case 2:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(facts, (route) => false);
+        bottomNavigationBar: BottomNavigationBar(
+            iconSize: 45,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.grass_outlined),
+                label: 'me',
+                backgroundColor: navbarColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart),
+                label: 'others',
+                backgroundColor: navbarColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.emoji_objects),
+                label: 'knowledge',
+                backgroundColor: navbarColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'profile',
+                backgroundColor: navbarColor,
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: iconColor,
+            onTap: (int index) {
+              switch (index) {
+                case 0:
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(myStats, (route) => false);
+                case 1:
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(welcomeRoute, (route) => false);
+                case 2:
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(facts, (route) => false);
 
-              case 3:
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(welcomeRoute, (route) => false);
-            }
-            setState(
-              () {
-                _selectedIndex = index;
-              },
-            );
-          }),
+                case 3:
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(welcomeRoute, (route) => false);
+              }
+              setState(
+                () {
+                  _selectedIndex = index;
+                },
+              );
+            }),
+      ),
     );
   }
 }
