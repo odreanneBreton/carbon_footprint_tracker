12dc2bf7aa923a607038758f9603cf0f1c7ff937
Odréanne Breton <odreanne.breton@gmail.com>
2024-02-04 02:41:58 -0500

    auth ui

--- carbon_app/lib/views/auth.dart
+++ carbon_app/lib/views/auth.dart
@@ -2,6 +2,7 @@ import 'package:carbon_app/constants/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:carbon_app/constants/color.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthView extends StatelessWidget {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -9,33 +10,53 @@ class AuthView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Authentication'),
-        backgroundColor: backgroundColor,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  Navigator.of(context).pushNamed(loginRoute);
-                },
-                child: Text('Sign In '),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  Navigator.of(context).pushNamed(createAccountRoute);
-                },
-                child: Text('Sign Up'),
-              ),
-            ],
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            height: 600,
+            width: 600,
           ),
-        ),
+          SizedBox(height: 10),
+          OutlinedButton(
+            onPressed: () {
+              // Handle register button press
+              Navigator.of(context).pushNamed(loginRoute);
+            },
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Color.fromARGB(255, 65, 154, 40)),
+              minimumSize: const Size(170, 60), // Adjust the size as needed
+            ),
+            child: Text(
+              'Login',
+              style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                color: Color.fromARGB(255, 37, 129, 37),
+                fontSize: 28, // Adjust the font size
+              )),
+            ),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {
+              // Handle register button press
+              Navigator.of(context).pushNamed(createAccountRoute);
+            },
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Color.fromARGB(255, 61, 160, 33)),
+              minimumSize: const Size(170, 60), // Adjust the size as needed
+            ),
+            child: Text(
+              'Register',
+              style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                color: Color.fromARGB(255, 40, 145, 40),
+                fontSize: 28, // Adjust the font size
+              )),
+            ),
+          ),
+        ],
       ),
     );
   }
