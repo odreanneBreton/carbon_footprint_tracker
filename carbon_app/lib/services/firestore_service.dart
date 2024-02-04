b69e84169ab6c3fd291094aa721c5208f379ef6b
Julien Racette <julienracette@pop-os.localdomain>
2024-02-04 09:55:59 -0500

    fonctions pour db

--- carbon_app/lib/services/firestore_service.dart
+++ carbon_app/lib/services/firestore_service.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:carbon_app/user.dart';
+import 'package:carbon_app/person.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
@@ -42,5 +42,44 @@ class FirestoreService {
 
   Future<void> updateCo2(String userId, int co2) async {
     await _firestore.collection('users').doc(userId).update({'co2': co2});
+  }
+
+  Future<List<Person>> getUsersSortedByCo2() async {
+    QuerySnapshot querySnapshot = await _firestore.collection('users').get();
+
+    List<Person> users = querySnapshot.docs.map((doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      return Person(
+        uid: doc.id,
+        money: data['money'] ?? 0,
+        co2: data['co2'] ?? 0,
+        name: data['name'] ?? 'default',
+      );
+    }).toList();
+
+    // Sort the users based on co2 attribute
+    users.sort((a, b) => b.co2.compareTo(a.co2));
+
+    return users;
+  }
+
+  Future<Person?> getCurrentUser() async {
+    User user = FirebaseAuth.instance.currentUser!;
+
+    if (user != null) {
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(user.uid).get();
+
+      if (userDoc.exists) {
+        Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+        return Person(
+          uid: user.uid,
+          money: userData['money'] ?? 0,
+          co2: userData['co2'] ?? 0,
+          name: userData['name'] ?? 'default',
+        );
+      }
+    }
+    return null;
   }
 }
