f2f0266fc2af4ed00d29151af3433cdcce294df1
Odréanne Breton <odreanne.breton@gmail.com>
2024-02-04 03:38:44 -0500

    register ui

--- carbon_app/lib/views/create_account.dart
+++ carbon_app/lib/views/create_account.dart
@@ -1,6 +1,7 @@
 import 'package:carbon_app/services/auth_service.dart';
 import 'package:carbon_app/views/welcome_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -17,15 +18,18 @@ class _CreateAccountState extends State<CreateAccount> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Account'),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 150,
+              width: 150,
+            ),
             SizedBox(
-              width: MediaQuery.of(context).size.width / 2,
+              width: MediaQuery.of(context).size.width / 1.5,
               child: TextField(
                 controller: _emailController,
                 decoration: const InputDecoration(hintText: 'Email'),
@@ -35,7 +39,7 @@ class _CreateAccountState extends State<CreateAccount> {
               height: 30.0,
             ),
             SizedBox(
-              width: MediaQuery.of(context).size.width / 2,
+              width: MediaQuery.of(context).size.width / 1.5,
               child: TextField(
                 controller: _passwordController,
                 obscureText: true,
@@ -47,7 +51,7 @@ class _CreateAccountState extends State<CreateAccount> {
             const SizedBox(
               height: 30.0,
             ),
-            ElevatedButton(
+            OutlinedButton(
               onPressed: () async {
                 final message = await AuthService().registration(
                   email: _emailController.text,
@@ -63,7 +67,18 @@ class _CreateAccountState extends State<CreateAccount> {
                   ),
                 );
               },
-              child: const Text('Create Account'),
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(color: Color.fromARGB(255, 65, 154, 40)),
+                minimumSize: const Size(125, 50), // Adjust the size as needed
+              ),
+              child: Text(
+                'Register',
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  color: Color.fromARGB(255, 37, 129, 37),
+                  fontSize: 25, // Adjust the font size
+                )),
+              ),
             ),
           ],
         ),
