d1c7075dc9cb1303f3666d7a3ee3419866051164
Fil Dessureault <fildessureault@gmail.com>
2024-02-03 23:00:02 -0500

    fix couleurs

--- carbon_app/lib/constants/color.dart
+++ carbon_app/lib/constants/color.dart
@@ -2,9 +2,9 @@
 //Cool et frais
 import 'package:flutter/material.dart';
 
-const backgroundColor = Color.fromARGB(255, 247, 253, 240);
-const headerColor = Color(0xff1c7c54);
-const letterColor = Color.fromARGB(255, 223, 255, 219);
-const navbarColor = Color(0xff1c7c54);
-const iconColor = Color(0xffb1cf5f);
-const postColor = Color.fromARGB(255, 227, 233, 211);
+const background_color = Color.fromARGB(255, 247, 253, 240);
+const header_color = Color(0xff1c7c54);
+const letter_color = Color.fromARGB(255, 223, 255, 219);
+const navbar_color = Color(0xff1c7c54);
+const icon_color = Color(0xffb1cf5f);
+const post_color = Color.fromARGB(255, 227, 233, 211);
